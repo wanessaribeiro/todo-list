3316bd749ebd9c09f5a8efe310373afc70765a70
unknown <wanessacpr@gmail.com>
2023-06-06 16:23:11 -0300

    todo-list with calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListModule } from './task-list/task-list.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TaskListModule,
+    CalendarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
